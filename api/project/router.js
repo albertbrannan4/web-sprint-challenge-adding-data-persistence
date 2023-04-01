07bba3355bd92c53b7dbc340fc40508f0af5f487
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-04-01 16:25:36 -0400

    router and server made with scripts

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,8 @@
 // build your `/api/projects` router here
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.json("project router works");
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,1 +1,14 @@
 // build your server here and require it from index.js
+const express = require("express");
+const ProjectRouter = require("./project/router");
+
+const server = express();
+
+server.use(express.json());
+server.use("/api/project", ProjectRouter);
+
+server.get("*", (req, res) => {
+  res.json("Page Not Found");
+});
+
+module.exports = server;
