68e84e8d58e971d69989ea677d71e36ad8b3737c
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-04-01 17:38:42 -0400

    converted completed

--- api/project/model.js
+++ api/project/model.js
@@ -3,7 +3,18 @@ const db = require("../../data/dbConfig");
 
 async function getAllProjects() {
   const result = await db("projects");
-  return result;
+  const convertCompleted = result.map((each) => {
+    let completed = false;
+    if (each.project_completed === 1) completed = true;
+    return {
+      project_id: each.project_id,
+      project_name: each.project_name,
+      project_description: each.project_description,
+      project_completed: completed,
+    };
+  });
+
+  return convertCompleted;
 }
 
 module.exports = {
