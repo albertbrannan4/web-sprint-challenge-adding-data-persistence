549fd95e604ecb5f97c4669bdbe83acd9ad2e657
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-04-01 16:57:21 -0400

    resource schema

--- data/migrations/20230401204309_first_migration.js
+++ data/migrations/20230401204309_first_migration.js
@@ -3,12 +3,18 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable("project", (table) => {
-    table.increments("project_id");
-    table.string("project_name", 128).notNullable();
-    table.string("project_description", 128);
-    table.boolean("project_completed").defaultTo(false);
-  });
+  return knex.schema
+    .createTable("project", (table) => {
+      table.increments("project_id");
+      table.string("project_name", 128).notNullable();
+      table.string("project_description", 128);
+      table.boolean("project_completed").defaultTo(false);
+    })
+    .createTable("resource", (table) => {
+      table.increments("resource_id");
+      table.string("resource_name", 128).notNullable().unique();
+      table.string("resource_description");
+    });
 };
 
 /**
@@ -16,5 +22,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("project");
+  return knex.schema.dropTableIfExists("project").dropTableIfExists("resource");
 };
