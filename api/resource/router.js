21c6e5b0267cb752446cb11c9b910303db87c841
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-04-01 20:55:50 -0400

    getting all resources

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,14 @@
 // build your `/api/resources` router here
+const router = require("express").Router();
+const Resource = require("./model");
+
+router.get("/", async (req, res) => {
+  try {
+    const allResources = await Resource.getAllResources();
+    res.json(allResources);
+  } catch (err) {
+    res.status(500).json({ message: "getting resources didnt work" });
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 // build your server here and require it from index.js
 const express = require("express");
 const ProjectRouter = require("./project/router");
+const ResourceRouter = require("./resource/router");
 
 const server = express();
 
 server.use(express.json());
 server.use("/api/projects", ProjectRouter);
+server.use("/api/resources", ResourceRouter);
 
 server.get("*", (req, res) => {
   res.status(404).json("Page Not Found");
